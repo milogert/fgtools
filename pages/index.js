13099e4165edfaacc44b481a4f7cfa9329364622
Milo Gertjejansen <milo@milogert.com>
2022-11-06 19:37:52 -0500

    Fix linting errors

--- pages/index.js
+++ pages/index.js
@@ -43,12 +43,12 @@ const HomePage = () =>
 
     <h3 className="text-2xl mt-3">FAQ</h3>
 
-    <h4 className="text-xl mt-3">My preview isn't printing properly!</h4>
+    <h4 className="text-xl mt-3">My preview isn&apos;t printing properly!</h4>
 
     <p className="mb-2">Step one, try printing in Google Chrome or Firefox. Safari has issues that I am stuck on and I semi-gave up on.</p>
 
     <p className="mb-2">
-      Step two, if the printing still doesn't appear correctly then please file a bug&nbsp;
+      Step two, if the printing still doesn&apos;t appear correctly then please file a bug&nbsp;
       <a
         href="https://github.com/milogert/frostgrave-sheet/issues/new"
         className="underline text-blue-500"
@@ -61,14 +61,14 @@ const HomePage = () =>
     <h4 className="text-xl mt-3">You are missing expansion X!</h4>
 
     <p className="mb-2">
-      It's because I don't have the book or I haven't played it yet. Feel free to&nbsp;
+      It&apos;s because I don&apos;t have the book or I haven&apos;t played it yet. Feel free to&nbsp;
       <a
         href="https://paypal.me/milogert"
         className="underline text-blue-500"
         target="_blank"
         rel="noreferrer"
       >send me money</a>
-      &nbsp;to buy the expansion. Once it arrives I'll add it and let you know!
+      &nbsp;to buy the expansion. Once it arrives I&apos;ll add it and let you know!
     </p>
 
     <h4 className="text-xl mt-3">Can you make the sheet fillable so I can print it and not have to write manually?</h4>
@@ -77,7 +77,7 @@ const HomePage = () =>
     <ul className="list-outside list-disc">
       <li className="ml-8">There is <i>a lot</i> more code and logic involved. My goal with this page is to keep it light.</li>
       <li className="ml-8">You frequently have to change entries for whole soldiers, so really the main thing that would be worth doing this for is the wizard school, and then you can just write it.</li>
-      <li className="ml-8">I wouldn't use this feature.</li>
+      <li className="ml-8">I wouldn&apos;t use this feature.</li>
       <li className="ml-8">I like the pen and paper asthetic.</li>
     </ul>
 
